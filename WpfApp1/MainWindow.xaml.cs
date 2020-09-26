d39e514a5682739f0827b84710b91dffe9baedd1
Nikita Pilinko <mineralsfree@gmail.com>
2020-09-26 19:56:52 +0300

    added stuff

---
+++ WpfApp1/MainWindow.xaml.cs
@@ -0,0 +1,133 @@
+﻿
+using System;
+using System.Windows;
+using Emgu.CV;
+
+namespace WpfApp1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private System.Windows.Threading.DispatcherTimer dispatcherTimer = new System.Windows.Threading.DispatcherTimer();
+        private VideoCapture _capture = null;
+        private bool _captureInProgress;
+        private bool record = false;
+        private bool _photo = false;
+        private VideoWriter writer = new VideoWriter(Environment.GetFolderPath(Environment.SpecialFolder.Desktop) + @"\" + "mov" + ".mp4", VideoWriter.Fourcc('M', 'P', '4', 'V'), 15F, new System.Drawing.Size(1920, 1080), false);
+
+        public MainWindow()
+        {
+            dispatcherTimer.Tick += dispatcherTimer_Tick;
+            dispatcherTimer.Interval = TimeSpan.FromMilliseconds(1000.0 / 20.0);
+            InitializeComponent();
+            CvInvoke.UseOpenCL = false;
+            try
+            {
+                Console.WriteLine("here");
+                _capture = new VideoCapture("rtsp://admin:123sakha@192.168.100.109:554/ISAPI/Streaming/Channels/101");
+                _capture.ImageGrabbed += ProcessFrame;
+            }
+            catch (NullReferenceException excpt)
+            {
+                MessageBox.Show(excpt.Message);
+            }
+        }
+        
+        
+private void dispatcherTimer_Tick(object sender, EventArgs e)
+        {
+            if (_capture != null)
+            {
+                Mat frame = new Mat();
+                _capture.Retrieve(frame, 0);
+                if (frame != null)
+                {
+                    Image<Emgu.CV.Structure.Gray, Byte> img = frame.ToImage<Emgu.CV.Structure.Gray, Byte>();
+                    try
+                    {
+                        if (record)
+                        {
+                            writer.Write(img.Mat);
+                        }
+                        captureImageBox.Source = helper.ToBitmapSource(img.Bitmap);
+                    } catch(Exception ex)
+                    {
+                        Console.WriteLine(ex.StackTrace);
+                    }
+
+                }
+            }
+        }
+        private void ProcessFrame(object sender, EventArgs arg)
+        {
+            Mat frame = new Mat();
+     
+            _capture.Retrieve(frame, 0);
+            try
+            {
+                var timeStamp = DateTimeOffset.UtcNow.ToUnixTimeSeconds();
+                var path = Environment.GetFolderPath(Environment.SpecialFolder.Desktop) + @"\" + "img" + timeStamp + ".bmp";
+                if (_photo)
+                {
+                    frame.ToImage<Emgu.CV.Structure.Gray, Byte>().Bitmap.Save(path);
+                }
+                _photo = false;
+               
+                
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+            }
+           // captureImageBox.Source = new BitmapImage( new Uri("lol.bmp"));
+
+
+        }
+        private void recordButton_Click(object sender, EventArgs e)
+        {
+            this.record = !this.record;
+            if (this.record)
+            {
+                RecordButton.Content = "Stop Recording";
+            } else
+            {
+                RecordButton.Content = "Start Recording";
+            }
+        }
+        private void photo(object sender, EventArgs e)
+        {
+            _photo = true;
+        }
+        private void captureButton_Click(object sender, EventArgs e)
+        {
+            dispatcherTimer.Start();
+            if (_capture != null)
+            {
+                if (_captureInProgress)
+                {  //stop the capture
+                    btnLoadFromFile.Content = "Start Capture";
+                    _capture.Pause();
+
+                }
+                else
+                {
+                    //start the capture
+                    btnLoadFromFile.Content = "Stop";
+                    _capture.Start();
+                }
+
+                _captureInProgress = !_captureInProgress;
+            }
+        }
+
+        void MainWindow_Closing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            writer.Dispose();
+        }
+
+
+    }
+
+}
---
+++ WpfApp1/helper.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Drawing;
+using System.Globalization;
+using System.IO;
+using System.Runtime.InteropServices;
+using System.Windows;
+using System.Windows.Data;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+
+namespace WpfApp1
+{
+    [ValueConversion(typeof(Bitmap), typeof(ImageSource))]
+    static class helper
+    {
+        // System.Runtime.InteropServices.dll
+        public static System.Drawing.Bitmap ToBitmap(this BitmapSource bitmapsource)
+        {
+            System.Drawing.Bitmap bitmap;
+            using (var outStream = new MemoryStream())
+            {
+                // from System.Media.BitmapImage to System.Drawing.Bitmap
+                BitmapEncoder enc = new BmpBitmapEncoder();
+                enc.Frames.Add(BitmapFrame.Create(bitmapsource));
+                enc.Save(outStream);
+                bitmap = new System.Drawing.Bitmap(outStream);
+                return bitmap;
+            }
+        }
+        public static BitmapSource ConvertBitmap(Bitmap source)
+        {
+            return System.Windows.Interop.Imaging.CreateBitmapSourceFromHBitmap(
+                          source.GetHbitmap(),
+                          IntPtr.Zero,
+                          Int32Rect.Empty,
+                          BitmapSizeOptions.FromWidthAndHeight(1920,1080));
+        }
+
+
+        [DllImport("gdi32")]
+        private static extern int DeleteObject(IntPtr o);
+
+        /// <summary>
+        /// Convert an IImage to a WPF BitmapSource. The result can be used in the Set Property of Image.Source
+        /// </summary>
+        /// <param name="image">The Emgu CV Image</param>
+        /// <returns>The equivalent BitmapSource</returns>
+        public static BitmapSource ToBitmapSource(Bitmap image)
+        {
+            using (System.Drawing.Bitmap source = image)
+            {
+                IntPtr ptr = source.GetHbitmap(); //obtain the Hbitmap
+
+                BitmapSource bs = System.Windows.Interop.Imaging.CreateBitmapSourceFromHBitmap(
+                    ptr,
+                    IntPtr.Zero,
+                    Int32Rect.Empty,
+                    System.Windows.Media.Imaging.BitmapSizeOptions.FromEmptyOptions());
+
+                DeleteObject(ptr); //release the HBitmap
+                return bs;
+            }
+        }
+
+    }
+}
